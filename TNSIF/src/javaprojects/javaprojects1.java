1ebc513ae538b4f80eb635eb0bbc66d9cf23d85d
N P <N P@NP>
2023-09-15 22:09:57 +0530

    Tnsif task 1

---
+++ TNSIF/src/javaprojects/javaprojects1.java
@@ -0,0 +1,11 @@
+package javaprojects;
+
+public class javaprojects1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HEllo Teammates");
+
+	}
+
+}
